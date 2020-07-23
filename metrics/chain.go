6ec174b8801538c863c371eded0e38c80dc00ea3
hexcola <hexcola@mail.com>
2020-07-23 19:06:48 +0800

    feat: 添加chain height 指标

--- metrics/chain.go
+++ metrics/chain.go
@@ -1,1 +1,13 @@
 package metrics
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var (
+	// Workers current number of workers
+	ChainHeight = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "chain_height",
+			Help: "current blockchain height of ",
+		},
+	)
+)
