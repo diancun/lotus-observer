b64a6dd93f2d268fe154c0fc1d0aaea0d2e2898a
hexcola <hexcola@mail.com>
2020-07-23 21:27:44 +0800

    fix: 将 ChainHeight 更新为Gauge

--- metrics/chain.go
+++ metrics/chain.go
@@ -4,8 +4,8 @@ import "github.com/prometheus/client_golang/prometheus"
 
 var (
 	// Workers current number of workers
-	ChainHeight = prometheus.NewCounter(
-		prometheus.CounterOpts{
+	ChainHeight = prometheus.NewGauge(
+		prometheus.GaugeOpts{
 			Name: "chain_height",
 			Help: "current blockchain height of ",
 		},
